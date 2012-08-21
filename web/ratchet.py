458419ac17d4d58131c5bc6c215b0bb363925d75
fabiobatalha <fabiobatalha@gmail.com>
2012-08-21 18:41:01 -0300

    Global Resource API

--- web/ratchet.py
+++ web/ratchet.py
@@ -13,6 +13,7 @@ from tornado.options import (
 from datetime import date
 import tornado
 import asyncmongo
+import urllib2
 
 define("port", default=8888, help="run on the given port", type=int)
 define("mongodb_port", default=27017, help="run MongoDB on the given port", type=int)
@@ -20,6 +21,7 @@ define("mongodb_host", default='localhost', help="run MongoDB on the given hostn
 define("mongodb_database", default='analytics', help="Record accesses on the given database")
 define("mongodb_max_connections", default=200, help="run MongoDB with the given max connections", type=int)
 define("mongodb_max_cached", default=20, help="run MongoDB with the given max cached", type=int)
+define("resources", default=None, help="indicates a txt file with api resources. Once this parameter is defined, the API will just work as accesses delivery.", type=str)
 
 
 class Application(tornado.web.Application):
@@ -39,6 +41,19 @@ class Application(tornado.web.Application):
             maxconnections=options.mongodb_max_connections,
             dbname=options.mongodb_database
         )
+
+        # Local is the default the default way that ratchet works.
+        self.api_style = 'local'
+
+        self.resources = []
+
+        if options.resources:
+            with open(options.resources) as f:
+                for line in f:
+                    self.resources.append(line)
+
+            if len(self.resources) > 0:
+                self.api_style = 'global'
 
         tornado.web.Application.__init__(self, handlers)
 
@@ -88,6 +103,22 @@ class ArticleHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         code = self.get_argument('code')
+
+        old_data = self.db.accesses.find_one({"code": code}, limit=1)
+        journal = old_data['journal']
+
+        if self.api_style == 'global':
+            self.db.accesses.remove({'code': code})
+            for resource in self.resources:
+                url = "http://%s/api/v1/journal?code=%s" % (resource, code)
+                data = urllib2.urlopen(url).read()
+                self.db.accesses.update(
+                    {'code': code},
+                    {'$set': {'type': 'article', 'journal': journal}, '$inc': data},
+                    safe=False,
+                    upsert=True
+                )
+
         self.db.accesses.find_one({"code": code}, limit=1, callback=self._on_get_response)
 
     def _on_get_response(self, response, error):
